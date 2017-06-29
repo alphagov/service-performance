7955c1bc99e1b1218eb028dce78ad127c582f736
Olly Legg <olly@51degrees.net>
2017-06-29 15:25:30 +0100

    Progressively enhance metrics filter panel.
    Submits the form when either of the fields changes. Hides the radio
    buttons, and makes them behave as links.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,2 @@
 //= require jquery
-
-$(document).ready(function() {
-  $('.metric-guidance').hide();
-  $('.show-guidance button').click(function() {
-    $(this).closest('.filter-cards li').find('.metric-guidance').toggle();
-    $(this).closest('.filter-cards li').find('.metric-name').toggleClass('bold');
-    $(this).find('span').toggle();
-  });
-});
+//= require metrics_filter_panel
