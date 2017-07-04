e9a4c57b3f84909dd2b69abf9120d54e37409d2c
Olly Legg <olly@51degrees.net>
2017-07-04 10:42:51 +0100

    Display a service's metrics on it's show page.
    It also includes a static time period selector, which'll be implemented
    later.

---
+++ app/presenters/service_metrics_presenter.rb
@@ -0,0 +1,13 @@
+class ServiceMetricsPresenter < MetricsPresenter
+  def has_departments?
+    false
+  end
+
+  def has_delivery_organisations?
+    false
+  end
+
+  def has_services?
+    false
+  end
+end
