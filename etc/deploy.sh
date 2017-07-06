f6c29994e75dbbf8d3cfa0cd8f75c63579308888
ambrozic <ambrozic@gmail.com>
2017-07-06 12:35:16 +0100

    Add restage and SECRET_KEY_BASE

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -16,4 +16,6 @@ wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key |
 echo "deb http://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
 sudo apt-get update && sudo apt-get install cf-cli
 cf login -u $PAAS_USER -p $PAAS_PASSWORD -a https://api.cloud.service.gov.uk -o cross-government-service-data -s $PAAS_SPACE
+cf set-env $PAAS_SERVICE SECRET_KEY_BASE $APP_SECRET_KEY_BASE
 cf push $PAAS_SERVICE --docker-image $DOCKER_REPOSITORY/$DOCKER_IMAGE:$TRAVIS_BUILD_NUMBER
+cf restage $PAAS_SERVICE
