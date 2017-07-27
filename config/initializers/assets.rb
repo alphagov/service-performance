ad124dfebaf904b72320c3e9a975c47924b2fff0
Paul Craig <paul_craig_16@hotmail.com>
2017-07-27 13:57:14 +0100

    Reference static assets from submodules
    Look for our static assets in our new submodules rather than the
    static files in product-page-template.
    Unfortunately, we've once again had to duplicate a CSS file to
    overwrite some file paths. fonts-ie8.css.erb is referencing some
    font files in a way that our asset path won't find them, so I've
    duplicated the file to update all the paths.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,11 +1,11 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile = [
   'application',
+  'fonts-ie8',
+  'print',
   'screen',
   'screen-old-ie',
   'product-image.png',
-  'product-page-template/fonts',
-  'product-page-template/fonts-ie8',
-  'print',
-  'product-page-template/gov.uk_logotype_crown_invert_trans.png'
+  'govuk_template/source/assets/stylesheets/fonts',
+  'govuk_template/source/assets/images/gov.uk_logotype_crown_invert_trans.png'
 ]
