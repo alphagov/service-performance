04bb8f793d886721f86221afcff04e914b4a0e1c
Olly Legg <olly@51degrees.net>
2017-07-27 11:47:33 +0100

    Add importer which updates a delivery organisation's department.
    Department's are a special case of delivery organisations, they're the parent,
    or top-level, organisation. The list of delivery organisations is imported from
    the Government Organisation Register, but this lacks the hierarchy information.
    
    In order to create the hierarchy, we supplement this information with our own,
    mapping of delivery organisation to department.
    
    This should be imported after the register has been updated, and it takes a CSV
    file (on stdin) in the following format:
    
       Organisation ID,Department ID
       D101,D1
       D30,D1
       D2,D2
    
    Usage:
    
       cat mapping.csv | rails runner 'DepartmentsImporter.new.import'

---
+++ app/importers/departments_importer.rb
@@ -0,0 +1,85 @@
+# Importing departments, and associating them with delivery organisations.
+#
+# Department's are a special case of delivery organisations, they're the parent,
+# or top-level, organisation. The list of delivery organisations is imported from
+# the Government Organisation Register, but this lacks the hierarchy information.
+#
+# In order to create the hierarchy, we supplement this information with our own,
+# mapping of delivery organisation to department.
+#
+# This should be imported after the register has been updated, and it takes a CSV
+# file (on stdin) in the following format:
+#
+#    Organisation ID,Department ID
+#    D101,D1
+#    D30,D1
+#    D2,D2
+#
+# Usage:
+#
+#.   cat mapping.csv | rails runner 'DepartmentsImporter.new.import'
+
+require 'csv'
+
+class DepartmentsImporter
+  def import(input = ARGF, output = $stderr)
+    ActiveRecord::Base.transaction do
+      department_ids = Set.new
+      department_ids_by_organisation = {}
+
+      csv = CSV.new(input, headers: true)
+      csv.each do |row|
+        organisation_id, department_id = row['Organisation ID'], row['Department ID']
+        next if organisation_id.nil? || department_id.nil?
+
+        department_ids << department_id
+        department_ids_by_organisation[organisation_id] = department_id
+      end
+
+      department_ids.each do |department_id|
+        delivery_organisation = DeliveryOrganisation.where(natural_key: department_id).first!
+
+        department = Department.where(natural_key: department_id).first_or_initialize
+        department.name = delivery_organisation.name
+        department.website = delivery_organisation.website
+
+        log = ->(message) do
+          output.puts message % {key: department.natural_key, name: department.name}
+        end
+
+        case
+        when department.new_record?
+          log.("promoting delivery organisation to department: key=%{key}, name=%{name}")
+        when department.changed?
+          log.("updating department: key=%{key}, name=%{name}")
+        else
+          log.("ignoring department, no changes: key=%{key}, name=%{name}")
+        end
+          
+        department.save!
+      end
+
+      department_ids_by_organisation.each do |organisation_id, department_id|
+        department = Department.where(natural_key: department_id).first!
+        delivery_organisation = DeliveryOrganisation.where(natural_key: organisation_id).first
+
+        log = ->(message, args = nil) do
+          args ||= {key: delivery_organisation.natural_key, department_code: delivery_organisation.department_code}
+          output.puts message % args
+        end
+
+        if delivery_organisation
+          delivery_organisation.department = department
+          if delivery_organisation.changed?
+            log.("updating delivery organisation's department: key=%{key}, department_code=%{department_code}")
+          else
+            log.("ignoring delivery organisation, no changes: key=%{key}, department_code=%{department_code}")
+          end
+          delivery_organisation.save!
+        else
+          log.("unknown delivery organisation: key=%{key}", key: organisation_id)
+        end
+      end
+    end
+  end
+end
