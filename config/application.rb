8ccee95990e149a0dc62ae026bf8a8d46402b2d4
Ross Jones <ross@servercode.co.uk>
2017-09-08 12:49:47 +0100

    Submodules and basic styling

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,27 @@
 require_relative 'boot'
 
-require 'rails/all'
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "action_controller/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module GsdPublishData
+module CgsdPublishData
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.generators.system_tests = nil
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths += [
+      config.root.join('app', 'controllers', 'metrics')
+    ]
+
+    config.assets.paths += [
+      config.root.join('vendor', 'submodules'),
+      # govuk-elements-sass is in a subdirectory of govuk_elements
+      config.root.join('vendor', 'submodules', 'govuk_elements', 'packages')
+    ]
   end
 end
