4f0bbc79b80d04cbd6da2ce5fc99b0a1e97cf92d
Olly Legg <olly@51degrees.net>
2017-09-13 11:38:06 +0100

    Configure services admin dashboard.

---
+++ app/dashboards/service_dashboard.rb
@@ -0,0 +1,66 @@
+require "administrate/base_dashboard"
+
+class ServiceDashboard < Administrate::BaseDashboard
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    id: Field::Number,
+    name: Field::String,
+    purpose: Field::Text,
+    how_it_works: Field::Text,
+    typical_users: Field::Text,
+    frequency_used: Field::Text,
+    duration_until_outcome: Field::Text,
+    start_page_url: Field::String,
+    paper_form_url: Field::String,
+  }.freeze
+
+  # COLLECTION_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to add, remove, or rearrange items.
+  COLLECTION_ATTRIBUTES = [
+    :id,
+    :name,
+  ].freeze
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = [
+    :id,
+    :name,
+    :purpose,
+    :how_it_works,
+    :typical_users,
+    :frequency_used,
+    :duration_until_outcome,
+    :start_page_url,
+    :paper_form_url,
+  ].freeze
+
+  # FORM_ATTRIBUTES
+  # an array of attributes that will be displayed
+  # on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = [
+    :name,
+    :purpose,
+    :how_it_works,
+    :typical_users,
+    :frequency_used,
+    :duration_until_outcome,
+    :start_page_url,
+    :paper_form_url,
+  ].freeze
+
+  # Overwrite this method to customize how services are displayed
+  # across all pages of the admin dashboard.
+  #
+  # def display_resource(service)
+  #   "Service ##{service.id}"
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   namespace :admin do
+    resources :services
     resources :users
 
-    root to: "users#index"
+    root to: "services#index"
   end
 
   devise_for :users
