277da8dc76aa9f5884c83cc0785c379b4babdc39
Ross Jones <ross@servercode.co.uk>
2017-09-22 10:43:37 +0100

    Force HTTPS for tag manager requests

--- app/helpers/google_tag_manager_helper.rb
+++ app/helpers/google_tag_manager_helper.rb
@@ -5,7 +5,7 @@ module GoogleTagManagerHelper
       <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
       })(window,document,'script','dataLayer','#{tag_manager_code}');</script>
      }.strip.html_safe
   end
@@ -13,7 +13,7 @@ module GoogleTagManagerHelper
   def tag_manager_nojs
     return '' if !tag_manager_code
     %{
-      <noscript><iframe src="//www.googletagmanager.com/ns.html?id=#{tag_manager_code}"
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=#{tag_manager_code}"
       height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
      }.strip.html_safe
   end
