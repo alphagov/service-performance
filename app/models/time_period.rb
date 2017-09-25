260f3652af9d41a43dea69c7e9e85baea70d98d4
Ross Jones <ross@servercode.co.uk>
2017-09-25 15:30:32 +0100

    Reset the default time-period

--- app/models/time_period.rb
+++ app/models/time_period.rb
@@ -1,10 +1,10 @@
 class TimePeriod
-  DEFAULT_TIME_PERIOD_DURATION = 6.months
+  DEFAULT_TIME_PERIOD_DURATION = 12.months
   DEFAULT_TIME_PERIOD_LAG = 2.months
 
   def self.default
-    ends_on = Date.new(2017, 6, 30)  # (Date.today - DEFAULT_TIME_PERIOD_LAG).end_of_month
-    starts_on = Date.new(2017, 1, 1) # (ends_on - DEFAULT_TIME_PERIOD_DURATION + 1.day)
+    ends_on = (Date.today - DEFAULT_TIME_PERIOD_LAG).end_of_month
+    starts_on = (ends_on - DEFAULT_TIME_PERIOD_DURATION + 1.day)
 
     new(starts_on, ends_on)
   end
