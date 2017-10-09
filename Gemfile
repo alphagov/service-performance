3f19d45ece434c8f72e35ab60fd6994ebfc8452e
Olly Legg <olly@51degrees.net>
2017-10-09 13:45:33 +0100

    Update Rails to v5.1.4.

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ end
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.0.2'
+gem 'rails', '~> 5.1.4'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- db/schema.rb
+++ db/schema.rb
@@ -15,79 +15,79 @@ ActiveRecord::Schema.define(version: 20170920140707) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "calls_received_metrics", force: :cascade do |t|
-    t.string   "department_code",            null: false
-    t.string   "delivery_organisation_code"
-    t.string   "service_code",               null: false
-    t.date     "starts_on",                  null: false
-    t.date     "ends_on",                    null: false
-    t.integer  "quantity"
-    t.boolean  "sampled",                    null: false
-    t.integer  "sample_size"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.string   "item",                       null: false
+  create_table "calls_received_metrics", id: :serial, force: :cascade do |t|
+    t.string "department_code", null: false
+    t.string "delivery_organisation_code"
+    t.string "service_code", null: false
+    t.date "starts_on", null: false
+    t.date "ends_on", null: false
+    t.integer "quantity"
+    t.boolean "sampled", null: false
+    t.integer "sample_size"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "item", null: false
   end
 
-  create_table "delivery_organisations", force: :cascade do |t|
-    t.string   "natural_key",     null: false
-    t.string   "name",            null: false
-    t.string   "website",         null: false
-    t.string   "department_code"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.index ["natural_key"], name: "index_delivery_organisations_on_natural_key", unique: true, using: :btree
+  create_table "delivery_organisations", id: :serial, force: :cascade do |t|
+    t.string "natural_key", null: false
+    t.string "name", null: false
+    t.string "website", null: false
+    t.string "department_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["natural_key"], name: "index_delivery_organisations_on_natural_key", unique: true
   end
 
-  create_table "departments", force: :cascade do |t|
-    t.string   "natural_key", null: false
-    t.string   "name",        null: false
-    t.string   "website",     null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["natural_key"], name: "index_departments_on_natural_key", unique: true, using: :btree
+  create_table "departments", id: :serial, force: :cascade do |t|
+    t.string "natural_key", null: false
+    t.string "name", null: false
+    t.string "website", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["natural_key"], name: "index_departments_on_natural_key", unique: true
   end
 
-  create_table "services", force: :cascade do |t|
-    t.string   "natural_key",                null: false
-    t.string   "name",                       null: false
-    t.string   "hostname",                   null: false
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.string   "department_code",            null: false
-    t.string   "delivery_organisation_code"
-    t.text     "purpose"
-    t.text     "how_it_works"
-    t.text     "typical_users"
-    t.text     "frequency_used"
-    t.text     "duration_until_outcome"
-    t.string   "start_page_url"
-    t.string   "paper_form_url"
-    t.index ["natural_key"], name: "index_services_on_natural_key", unique: true, using: :btree
+  create_table "services", id: :serial, force: :cascade do |t|
+    t.string "natural_key", null: false
+    t.string "name", null: false
+    t.string "hostname", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "department_code", null: false
+    t.string "delivery_organisation_code"
+    t.text "purpose"
+    t.text "how_it_works"
+    t.text "typical_users"
+    t.text "frequency_used"
+    t.text "duration_until_outcome"
+    t.string "start_page_url"
+    t.string "paper_form_url"
+    t.index ["natural_key"], name: "index_services_on_natural_key", unique: true
   end
 
-  create_table "transactions_received_metrics", force: :cascade do |t|
-    t.string   "department_code",            null: false
-    t.string   "delivery_organisation_code"
-    t.string   "service_code",               null: false
-    t.date     "starts_on",                  null: false
-    t.date     "ends_on",                    null: false
-    t.string   "channel",                    null: false
-    t.integer  "quantity"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+  create_table "transactions_received_metrics", id: :serial, force: :cascade do |t|
+    t.string "department_code", null: false
+    t.string "delivery_organisation_code"
+    t.string "service_code", null: false
+    t.date "starts_on", null: false
+    t.date "ends_on", null: false
+    t.string "channel", null: false
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "transactions_with_outcome_metrics", force: :cascade do |t|
-    t.string   "department_code",            null: false
-    t.string   "delivery_organisation_code"
-    t.string   "service_code",               null: false
-    t.date     "starts_on",                  null: false
-    t.date     "ends_on",                    null: false
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.string   "outcome",                    null: false
-    t.integer  "quantity"
+  create_table "transactions_with_outcome_metrics", id: :serial, force: :cascade do |t|
+    t.string "department_code", null: false
+    t.string "delivery_organisation_code"
+    t.string "service_code", null: false
+    t.date "starts_on", null: false
+    t.date "ends_on", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "outcome", null: false
+    t.integer "quantity"
   end
 
   add_foreign_key "calls_received_metrics", "delivery_organisations", column: "delivery_organisation_code", primary_key: "natural_key"
