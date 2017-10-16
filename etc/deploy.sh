e906a4fc85d1f27d414fdbc1c0ab58a671087771
Ross Jones <ross@servercode.co.uk>
2017-10-16 10:30:41 +0100

    Do not ask about installing plugin, just install it

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -11,7 +11,7 @@ fi
 PAAS_SPACE=$1
 cf login -u $PAAS_USER -p $PAAS_PASSWORD -a https://api.cloud.service.gov.uk -o government-service-data -s $PAAS_SPACE
 cf add-plugin-repo CF-Community https://plugins.cloudfoundry.org
-cf install-plugin blue-green-deploy -r CF-Community
+cf install-plugin blue-green-deploy -f -r CF-Community
 
 cf bind-service $PAAS_SERVICE cgsd-pg-service
 cf set-env $PAAS_SERVICE SECRET_KEY_BASE $APP_SECRET_KEY_BASE
