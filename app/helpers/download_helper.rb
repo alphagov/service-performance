539bf33dc0d6c40a215781ef1935087da578aceb
Ross Jones <ross@servercode.co.uk>
2017-10-17 13:32:22 +0100

    Adds links to CSV download for each relevant page

---
+++ app/helpers/download_helper.rb
@@ -0,0 +1,18 @@
+module DownloadHelper
+  def download_link
+    path = request.path.split('/')
+    return '' if path[1] != "performance-data"
+
+    # Strip the blank entry and the performance-data entry
+    path.shift(2)
+    return build_link("/v1/data/government/download.csv") if path[0] == "government"
+
+    build_link("/v1/data/#{path[0]}/#{path[1]}/download.csv")
+  end
+
+private
+
+  def build_link(path)
+    URI::join(ENV.fetch('API_URL'), path)
+  end
+end
