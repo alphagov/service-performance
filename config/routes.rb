31d17aaa62aedd67cd3b2bed7632173a831c2f6e
Olly Legg <olly@51degrees.net>
2017-11-09 14:38:59 +0000

    fixup! Refactor CSV export to use MetricsCSVExporter

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,19 @@ Rails.application.routes.draw do
   scope :v1 do
     scope 'data' do
       resource :government, only: [:show] do
-        resources :metrics, only: [:index], controller: 'government_metrics'
+        resources :metrics, only: [:index], controller: 'government_metrics', defaults: { format: 'json' }
       end
 
       resources :departments, only: [:index, :show], controller: 'departments' do
-        resources :metrics, only: [:index], controller: 'department_metrics'
+        resources :metrics, only: [:index], controller: 'department_metrics', defaults: { format: 'json' }
       end
 
       resources :delivery_organisations, only: [:index, :show], controller: 'delivery_organisations' do
-        resources :metrics, only: [:index], controller: 'delivery_organisation_metrics'
+        resources :metrics, only: [:index], controller: 'delivery_organisation_metrics', defaults: { format: 'json' }
       end
 
       resources :services, only: [:index, :show], controller: 'services' do
-        resources :metrics, only: [:index], controller: 'service_metrics'
+        resources :metrics, only: [:index], controller: 'service_metrics', defaults: { format: 'json' }
       end
     end
   end
