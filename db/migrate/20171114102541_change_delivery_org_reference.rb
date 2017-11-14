2ea4e9c12163c84279cedbd999fbeba65100f8f4
Ross Jones <ross@servercode.co.uk>
2017-11-14 16:08:52 +0000

    Use the execute command

--- db/migrate/20171114102541_change_delivery_org_reference.rb
+++ db/migrate/20171114102541_change_delivery_org_reference.rb
@@ -3,8 +3,7 @@ class ChangeDeliveryOrgReference < ActiveRecord::Migration[5.1]
     add_column :services, :delivery_organisation_id, :integer, null: true
     add_foreign_key :services, :delivery_organisations, column: :delivery_organisation_id, primary_key: :id
 
-    sql = 'update services set delivery_organisation_id=(select id from delivery_organisations D where D.natural_key="services".delivery_organisation_code);'
-    ActiveRecord::Base.connection.execute(sql)
+    execute 'update services set delivery_organisation_id=(select id from delivery_organisations D where D.natural_key="services".delivery_organisation_code);'
 
     change_column :services, :delivery_organisation_id, :integer, null: false
     remove_column :services, :delivery_organisation_code, :string, null: false
