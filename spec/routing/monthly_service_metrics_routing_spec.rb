08cb93f39721dcfb9fbdfd479855e765e8273193
Olly Legg <olly@51degrees.net>
2017-11-20 16:15:23 +0000

    Fix routing specs.

--- spec/routing/monthly_service_metrics_routing_spec.rb
+++ spec/routing/monthly_service_metrics_routing_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'monthly service metrics routing', type: :routing do
   specify 'GET MonthlyServiceMetrics#edit' do
-    expect(get: '/publish/services/001/metrics/2017/09').to route_to(
-      controller: 'monthly_service_metrics',
+    expect(get: '/publish-data/services/001/metrics/2017/09').to route_to(
+      controller: 'publish_data/monthly_service_metrics',
       action: 'edit',
       service_id: '001',
       year: '2017',
@@ -12,8 +12,8 @@ RSpec.describe 'monthly service metrics routing', type: :routing do
   end
 
   specify 'PATCH MonthlyServiceMetrics#edit' do
-    expect(patch: '/publish/services/001/metrics/2017/09').to route_to(
-      controller: 'monthly_service_metrics',
+    expect(patch: '/publish-data/services/001/metrics/2017/09').to route_to(
+      controller: 'publish_data/monthly_service_metrics',
       action: 'update',
       service_id: '001',
       year: '2017',
