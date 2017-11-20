2c112d8d3c748a47c71cce46a6d118a7c5c38070
Ross Jones <ross@servercode.co.uk>
2017-11-20 14:40:38 +0000

    Use publish_service_metrics_url for absolute URL

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,2 @@
 module ApplicationHelper
-  def get_port
-    return "" if request.port.in? [80, 443]
-    ":#{request.port}"
-  end
-
-  def absolute_url(url)
-    "#{request.protocol}#{request.host}#{get_port}#{url}"
-  end
 end
