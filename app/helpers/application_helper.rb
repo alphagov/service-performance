ab4e005966c02a72ae8c4128ba025e882eae4dba
Ross Jones <ross@servercode.co.uk>
2017-11-20 13:44:53 +0000

    Add batch task to generate (show) magic URLS
    Added a new batch action for services that will show the user the
    magic links for a service.  For now this shows the previous month (just
    in case), the current month and 2 months in advance.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def get_port
+    return "" if request.port.in? [80, 443]
+    ":#{request.port}"
+  end
+
+  def absolute_url(url)
+    "#{request.protocol}#{request.host}#{get_port}#{url}"
+  end
 end
