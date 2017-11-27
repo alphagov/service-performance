b8258603b71bf11351831e16e5c91019c11bb3f7
Ross Jones <ross@servercode.co.uk>
2017-11-27 15:21:25 +0000

    Zero downtime deploy

---
+++ etc/deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ -z "$1" ]; then
+    echo "Missing PAAS space argument"
+    echo "  deploy.sh staging|production"
+    exit 1
+fi
+
+PAAS_SPACE=$1
+
+wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
+echo "deb http://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
+sudo apt-get update
+sudo apt-get install cf-cli
+
+cf login -u $PAAS_USERNAME -p $PAAS_PASSWORD -a https://api.cloud.service.gov.uk -o government-service-data -s $PAAS_SPACE
+cf add-plugin-repo CF-Community https://plugins.cloudfoundry.org
+cf install-plugin blue-green-deploy -r CF-Community
+
+cf set-env service-performance-$PAAS_SPACE SECRET_KEY_BASE $SECRET_KEY_BASE
+cf set-env service-performance-$PAAS_SPACE TAG_MANAGER_ID $TAG_MANAGER_ID
+
+cf blue-green-deploy service-performance-$PAAS_SPACE
+cf map-route service-performance-staging  cloudapps.digital  --hostname gsd-api
+cf map-route service-performance-staging  cloudapps.digital  --hostname gsd-view-data
+cf map-route service-performance-staging  cloudapps.digital  --hostname gsd-publish-data
