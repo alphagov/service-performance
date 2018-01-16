3baf8492a545c5b40f2aae073453fc588d614a18
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2018-01-16 12:21:30 +0000

    Controller for time period submission
    - basic controller
    - validates referer
    - redirects user to referer if valid

--- app/controllers/view_data/time_period_controller.rb
+++ app/controllers/view_data/time_period_controller.rb
@@ -1,15 +1,32 @@
 module ViewData
   class TimePeriodController < ViewDataController
     def edit
+      @referer = previous_url
       @time_period = TimePeriod.default
     end
 
     def update
-      success = false
+      @time_period = TimePeriod.default
+      @referer = previous_url
+      success = true
       if success
-        # Redirect to previous
+        #TODO where do we want to go if success? Permit params
+        redirect_to @referer
       else
-        render 'view_data/time_period/edit'
+        render 'view_data/time_period/edit', referer: @referer
+      end
+    end
+
+    def previous_url
+      path = params[:next] || request.referer
+      return view_data_government_metrics_path if path.nil?
+
+      path = URI.parse(path).path
+      begin
+        _ = Rails.application.routes.recognize_path(path)
+        path
+      rescue
+        view_data_government_metrics_path
       end
     end
   end
---
+++ spec/features/submitting_time_periods_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+RSpec.feature 'submitting time period data' do
+  let(:time_period) { TimePeriod.default }
+  let(:department) { FactoryGirl.create(:department, name: 'Department for Environment, Food & Rural Affairs') }
+  let(:delivery_organisation) { FactoryGirl.create(:delivery_organisation, department: department, name: 'Environment Agency') }
+  let(:service) { FactoryGirl.create(:service, :transactions_received_not_applicable, :calls_received_not_applicable, delivery_organisation: delivery_organisation, name: 'Flood Information Service') }
+  let(:create_metrics) {
+    month = time_period.start_month
+    6.times do
+      FactoryGirl.create(:monthly_service_metrics, :published, service: service, month: month, transactions_processed: 100, transactions_processed_with_intended_outcome: 100)
+      month = month.succ
+    end
+  }
+  specify 'submitting time data via a referer' do
+    create_metrics
+    visit view_data_government_metrics_path(group_by: Metrics::GroupBy::Service)
+    click_link('Data for time period')
+
+    expect(page).to have_text('What time period do you want to view?')
+
+    choose('range_12')
+    click_button 'Change dates'
+
+    expect(page).to have_current_path('/performance-data/government/metrics/service')
+    expect(page).to have_text('Find service')
+  end
+
+  specify 'submitting time data via no referer' do
+    create_metrics
+
+    visit view_data_time_period_path
+    expect(page).to have_text('What time period do you want to view?')
+
+    choose('range_12')
+    click_button 'Change dates'
+
+    expect(page).to have_current_path(view_data_government_metrics_path(group_by: Metrics::GroupBy::Department))
+  end
+
+  specify 'submitting time data with an invalid path' do
+    create_metrics
+
+    referer = 'http://example.com/malicious'
+    Capybara.current_session.driver.header 'Referer', referer
+
+    visit view_data_time_period_path
+    expect(page).to have_text('What time period do you want to view?')
+
+    Capybara.current_session.driver.header 'Referer', nil
+
+    choose('range_12')
+    click_button 'Change dates'
+
+    expect(page).to have_current_path(view_data_government_metrics_path(group_by: Metrics::GroupBy::Department))
+  end
+end
