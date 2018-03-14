568efbc384b5556fe35fd400157b8fca8dabb08b
Ross Jones <ross@servercode.co.uk>
2018-03-14 10:15:59 +0000

    Add a link to magic link admin to generate a mailto
    This PR modifies the magic link admin page to include a mailto link.
    This link will launch the administrators mail client with the subject,
    recipient (where available) and content already filled in.
    
    As it isn't possible to make the content HTML in a mailto, we just provide the links
    and rely on the server to linkify if they really want to.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
 
   ActiveAdmin.routes(self)
-
   get "/demo", to: "demo#index"
 
   namespace :publish, path: 'publish', module: nil do
