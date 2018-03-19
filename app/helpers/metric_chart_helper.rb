b0a0aa60aed32efc4cd2dd3871c0a1cb89c3889c
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2018-03-19 12:01:22 +0000

    Small refactor
    - Store sum of current and previous metrics in instance vars

--- app/helpers/metric_chart_helper.rb
+++ app/helpers/metric_chart_helper.rb
@@ -1,19 +1,19 @@
 module MetricChartHelper
   def calculate_transaction_difference(current_metrics, previous_metrics, total_label)
-    @current_metrics = current_metrics
-    @previous_metrics = previous_metrics
+    @current_metrics = current_metrics.sum
+    @previous_metrics = previous_metrics.sum
     if increased?
-      "#{@current_metrics.sum - @previous_metrics.sum} (#{difference_increase_percentage} %) more #{total_label.gsub('Total transactions', 'transactions').downcase} than previous #{@previous_metrics.count} months".html_safe
+      "#{@current_metrics - @previous_metrics} (#{difference_increase_percentage} %) more #{total_label.gsub('Total transactions', 'transactions').downcase} than previous #{previous_metrics.count} months".html_safe
     elsif same_difference
-      "Same number of #{total_label.gsub('Total transactions', 'transactions').downcase} as previous #{@previous_metrics.count} months".html_safe
+      "Same number of #{total_label.gsub("Total transactions", "transactions").downcase} as previous #{previous_metrics.count} months".html_safe
     else
-      "#{@previous_metrics.sum - @current_metrics.sum} (#{difference_decrease_percentage} %) less #{total_label.gsub('Total transactions', 'transactions').downcase} than previous #{@previous_metrics.count} months".html_safe
+      "#{@previous_metrics - @current_metrics} (#{difference_decrease_percentage} %) less #{total_label.gsub('Total transactions', 'transactions').downcase} than previous #{previous_metrics.count} months".html_safe
     end
   end
 
   def set_li_arrow_direction(current_metrics, previous_metrics)
-    @current_metrics = current_metrics
-    @previous_metrics = previous_metrics
+    @current_metrics = current_metrics.sum
+    @previous_metrics = previous_metrics.sum
     return nil if same_difference
     if increased?
       "increase"
@@ -22,15 +22,15 @@ module MetricChartHelper
     end
   end
 
-private
+  private
 
   def same_difference
-    true if @current_metrics.sum == @previous_metrics.sum
+    true if @current_metrics == @previous_metrics
   end
 
   def increased?
     return nil if same_difference
-    if @current_metrics.sum > @previous_metrics.sum
+    if @current_metrics > @previous_metrics
       true
     else
       false
@@ -38,14 +38,14 @@ private
   end
 
   def difference_increase_percentage
-    difference = @current_metrics.sum.to_f - @previous_metrics.sum.to_f
-    percentage = difference / @current_metrics.sum.to_f * 100
+    difference = @current_metrics.to_f - @previous_metrics.to_f
+    percentage = difference / @current_metrics.to_f * 100
     percentage.round(2)
   end
 
   def difference_decrease_percentage
-    difference = @previous_metrics.sum.to_f - @current_metrics.sum.to_f
-    percentage = difference / @previous_metrics.sum.to_f * 100
+    difference = @previous_metrics.to_f - @current_metrics.to_f
+    percentage = difference / @previous_metrics.to_f * 100
     percentage.round(2)
   end
 end
